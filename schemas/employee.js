51c46e61c331e494e2c8deb856a300089cde62c4
jay-io <jay.dnb@outlook.in>
2022-07-31 01:10:06 +0530

    Add mongodb employee schema

---
+++ schemas/employee.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+require('dotenv').config('../.env');
+
+const employeeSchema = new mongoose.Schema({
+    meta: {
+        is_activated: {
+            type: Boolean,
+            required: true
+        }
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        min: 10,
+        max: 10
+    },
+    attendance: [{
+        type: String
+    }]
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+module.employeeModel = mongoose.model('employee', employeeSchema, 'employee-profiles');
