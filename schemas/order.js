c296884eb4059596eddb036ff46fccdc1068b8aa
jay-io <jay.dnb@outlook.in>
2022-08-02 01:12:48 +0530

    Add mongodb gscope-orders schema

---
+++ schemas/order.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const { getLocalTime } = require('../utils/local-time');
+const serviceable = require('./serviceable');
+require('dotenv').config('../.env');
+
+const orderSchema = new mongoose.Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    todo: {
+        dry_wash: [{ type: serviceable }],
+        formal_wash: [{ type: serviceable }],
+        steam_iron: [{ type: serviceable }]
+    },
+    status: {
+        accepted: {
+            state: {
+                type: Boolean,
+                default: true
+            },
+            time: {
+                type: String,
+                default: getLocalTime()
+            }
+        },
+        picked_up: {
+            state: {
+                type: Boolean,
+                default: false
+            },
+            time: {
+                type: String,
+                default: null
+            },
+            assignee: {
+                /* this field will contain a Mongo DBRef to a doc in employee Model 
+                to announce voluntary task assignment state by employee client */
+                type: mongoose.Schema.Types.ObjectId,
+                default: null
+            }
+        },
+        processing: {
+            state: {
+                type: Boolean,
+                default: false
+            },
+            time: {
+                type: String,
+                default: null
+            }
+        },
+        delivered: {
+            state: {
+                type: Boolean,
+                default: false
+            },
+            time: {
+                type: String,
+                default: null
+            },
+            assignee: {
+                /* this field will contain a Mongo DBRef to a doc in employee Model 
+                to announce voluntary task assignment state by employee client */
+                type: mongoose.Schema.Types.ObjectId,
+                default: null
+            }
+        }
+    }
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+exports.orderModel = mongoose.model('order', orderSchema, 'gscope-orders');
