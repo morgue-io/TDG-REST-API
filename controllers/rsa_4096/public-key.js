edd378b0da150be1a5293e2eadd46ae0e2c53d5c
jay-io <jay.dnb@outlook.in>
2022-09-28 02:59:05 +0530

    Add public key request endpoint controller

---
+++ controllers/rsa_4096/public-key.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+/* Function to export public key to Android and React clients */
+exports.exportPublicKey = (_, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'PUB_KEY [base64]',
+        pubkey: Buffer.from(fs.readFileSync('../../config/public_key.pem'), 'utf8').toString('base64')
+    });
+};
