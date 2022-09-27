8d10604ccc31a4a2239b0aac35b93c646da7f622
jay-io <jay.dnb@outlook.in>
2022-09-28 02:57:53 +0530

    Remove required:true validation

--- schemas/serviceable.js
+++ schemas/serviceable.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
-exports.serviceable = new mongoose.Schema({
+module.exports = {
     item: {
         type: String,
-        enum: ['blazer', 'shirt-and-tshirt', 'pant-and-trousers', 'saree', 'ladies-upper', 'ladies-lower', 'cloths-and-others'],
-        required: true
+        enum: ['blazer', 'shirt-and-tshirt', 'pant-and-trousers', 'saree', 'ladies-upper', 'ladies-lower', 'cloths-and-others']
     },
     quantity: {
         type: Number,
-        min: 0,
-        required: true
-    }
-});
+        min: 0
+    },
+    _id: false
+};
