cb379c0e8b4832f487cc3677bc5a5a529ce2434b
jay-io <jay.dnb@outlook.in>
2022-09-30 15:45:57 +0530

    Connect routes to middlewares and controllers

---
+++ routes/adm/index.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const admAuth = require('../../controllers/admin/adm-auth');
+const admEmpProfile = require('../../controllers/admin/adm-employee-profile');
+const admEmpRegister = require('../../controllers/admin/adm-employee-register');
+const admGscopeOrders = require('../../controllers/admin/adm-gscope-orders');
+const admLogin = require('../../controllers/admin/adm-login');
+const admRegister = require('../../controllers/admin/adm-register');
+const admProfile = require('../../controllers/admin/adm-profile');
+
+router.route('/login')
+    .post(admLogin.adminLoginHandler);
+
+router.route('/logout')
+    .post(admAuth.authorisationHandler, admLogin.adminLogoutHandler);
+
+router.route('/register')
+    .post(admRegister.registrationHandler);
+
+router.route('/deregister')
+    .post(admAuth.authorisationHandler, admRegister.deregistrationHandler);
+
+router.route('/change-credentials')
+    .post(admAuth.authorisationHandler, admProfile.postUserCredentialHandler);
+
+router.route('/employee-register')
+    .post(admAuth.authorisationHandler, admEmpRegister.registrationHandler);
+
+router.route('/employee-deregister')
+    .post(admAuth.authorisationHandler, admProfile.deregistrationHandler);
+
+router.route('/employee-view')
+    .get(admAuth.authorisationHandler, admEmpProfile.getEmployeeProfileHandler);
+
+router.route('/orders')
+    .get(admAuth.authorisationHandler, admGscopeOrders.getGscopeOrdersHandler)
+    .post(admAuth.authorisationHandler, admGscopeOrders.postGscopeOrdersStatusHandler);
+
+module.exports = router;
