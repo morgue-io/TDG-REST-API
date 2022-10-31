a90d6a08396a128eb0a9d6a62ec0bc203b4f25f5
jay-io <jay.dnb@outlook.in>
2022-11-01 03:49:29 +0530

    Enable self-signed ssl encryption

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 /* imports */
 const express = require('express');
+const https = require('https');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const fs = require('fs');
 
 const { sweepRefToken } = require('./subroutines/rtoken-sweep');
 const { connectdb } = require('./config/mongodb');
@@ -23,8 +25,14 @@ app.use('/api/v1', require('./routes/index'));
 /* echo hello */
 app.get('/', async (_, res) => res.send('<h3 style="font-family: monospace">Acknowledged, ECHO 3</h3>'));
 
-app.listen(process.env.PORT, () => {
-    console.log(`🗸 Server listening on PORT ${process.env.PORT}`);
+// Listen http port
+const httpsServer = https.createServer({
+  key: fs.readFileSync('ssl/server.key'),
+  cert: fs.readFileSync('ssl/server.crt'),
+}, app);
+
+httpsServer.listen(443, () => {
+    console.log('🗸 Listening on port 443');
 });
 
 interpret();
