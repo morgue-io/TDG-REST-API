36a6e8a6bb13039d2f14494ffa531f89083e634a
jay-io <jay.dnb@outlook.in>
2022-11-03 12:46:21 +0530

    Integrate node-fetch to keepAlive

--- subroutines/keep-alive.js
+++ subroutines/keep-alive.js
@@ -1,11 +1,17 @@
 const { CronJob } = require('cron');
 const { getLocalTime } = require('../utils/local-time');
+const fetch = require('node-fetch')
 
 exports.keepAlive = new CronJob(
         /* run routine every 5 minutes */
         '*/5 * * * *',
         async () => {
-                console.log(`[${getLocalTime()}] Keeping alive...`)
+            try {
+                var res = await fetch('https://thedhobighat.co.in', { method: 'GET' });
+                console.log(`[${getLocalTime()}] [${res.status}] Keeping alive...`);
+            } catch (e) {
+                console.error(e)
+            }
         },
         null,
         false,
